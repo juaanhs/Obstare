656b4e57226363e4f80d63ed462c6a422ff4a457
juaanhs <ruaanhs@gmail.com>
2019-08-30 16:27:30 -0300

    TODO

---
+++ app/src/main/java/br/com/juaanhs/obstare/ui/activity/CalculaBishopActivity.java
@@ -0,0 +1,29 @@
+package br.com.juaanhs.obstare.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import br.com.juaanhs.obstare.R;
+
+public class CalculaBishopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calcula_bishop);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home :
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
